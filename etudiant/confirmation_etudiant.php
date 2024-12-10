0318f7ad90470d45afe4dbbedae3a8253c3c4a9f
MED Jasser Hamrouni <161141531+JasserPy@users.noreply.github.com>
2024-12-10 23:06:39 +0100

    Update confirmation_etudiant.php

--- etudiant/confirmation_etudiant.php
+++ etudiant/confirmation_etudiant.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inscription d'un visiteur</title>
+    <title>INSCRIPTION</title>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../styles/style.css">
 </head>
@@ -14,9 +14,9 @@
         <div class="form-container sign-in">
             <form action="../loginout/loginoutp.php">
                 <img src="../img/student.png" alt="Student" class="mb-4" style="width: 100px;">
-                <h2 style=color:green;>Bienvenue chez nous</h2>
-                <p>Nous sommes ravis de vous accueillir !</p>
-                <button>Retour</button>
+                <h2 style=color:green;>Welcome to us</h2>
+                <p>We are delighted to welcome you !</p>
+                <button>BACK</button>
             </form>
         </div>
     </div>
