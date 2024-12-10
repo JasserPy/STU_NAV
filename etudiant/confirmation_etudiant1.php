3bae160982698f4d4a8d0217f9c2075b3558b7c7
MED Jasser Hamrouni <161141531+JasserPy@users.noreply.github.com>
2024-12-10 23:08:28 +0100

    Update confirmation_etudiant1.php

--- etudiant/confirmation_etudiant1.php
+++ etudiant/confirmation_etudiant1.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inscription d'un visiteur</title>
+    <title>INSCRIPTION</title>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../styles/style.css">
 </head>
@@ -14,8 +14,8 @@
         <div class="form-container sign-in">
             <form action="../loginout/loginoutp.php">
                 <img src="../img/student.png" alt="Student" class="mb-4" style="width: 100px;">
-                <h3 style=color:green;>Les modifications ont été enregistrées avec succès</h3>
-                <button>Retour</button>
+                <h3 style=color:green;>Changes were saved successfully !</h3>
+                <button>BACK</button>
             </form>
         </div>
     </div>
